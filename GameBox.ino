1438bdcc3951e3bdbe3e0b5f8503eb746ecdf730
Hadi <hadi.ghasemizadeh84@gmail.com>
2017-04-24 18:02:07 +0100

    First commit gameBox project

---
+++ GameBox.ino
@@ -0,0 +1,29 @@
+int P1_OP;
+int P1_Red_LED;
+int P1_Green_LED;
+
+int P2_OP;
+int P2_Red_LED;
+int P2_Green_LED;
+
+int P3_OP;
+int P3_Red_LED;
+int P3_Green_LED;
+
+int P4_OP;
+int P4_Red_LED;
+int P4_Green_LED;
+
+int GB_Yellow_LED;
+int Speaker_Op;
+int GB_Number_PL_In;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
