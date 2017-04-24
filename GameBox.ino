6dbb439d822a9940bee42737b8d75988a9f209ea
Hadi <hadi.ghasemizadeh84@gmail.com>
2017-04-24 18:54:02 +0100

    Add piches file.

--- GameBox.ino
+++ GameBox.ino
@@ -1,3 +1,23 @@
+// Constant header file
+#include "Piches.h"
+
+// ================================> Lose <============================================\\
+int loseMelody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+int loseNoteDurations[] = { 4, 8, 8, 4, 4, 4, 4, 4};
+
+// ================================> Random selection <================================\\
+int randomMelody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+int randomNoteDurations[] = { 4, 8, 8, 4, 4, 4, 4, 4};
+
+// ================================> Game Box is winner <==============================\\
+int gbiwMelody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+int gbiwNoteDurations[] = { 4, 8, 8, 4, 4, 4, 4, 4};
+
+// ================================> player is winner <================================\\
+int piwMelody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+int piwNoteDurations[] = { 4, 8, 8, 4, 4, 4, 4, 4};
+// ====================================================================================\\
+
 int P1_OP;
 int P1_Red_LED;
 int P1_Green_LED;
@@ -26,4 +46,24 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+// Play lose melody
+void LoseMelodyPlay() {
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
 }
